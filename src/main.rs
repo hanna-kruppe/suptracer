a3fb90aafd83bb42242b84a016c2f4eeb57d7989
Robin Kruppe <robin.kruppe@gmail.com>
2016-06-13 15:32:02 +0200

    Teensy tiny bit cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,78 @@
 extern crate bmp;
 extern crate cgmath;
+extern crate conv;
 extern crate scoped_threadpool;
+extern crate stopwatch;
 extern crate obj;
+extern crate watertight_triangle;
 
+use std::fmt;
 use std::fs::File;
 use std::f32;
-use std::io::{BufReader, Write, stdout};
+use std::io::BufReader;
+use std::time::Duration;
+use std::path::Path;
 use std::sync::{Arc, Mutex};
 use cgmath::{Vector3, InnerSpace, vec3};
 use scoped_threadpool::Pool;
+use stopwatch::Stopwatch;
 
-#[derive(Copy, Clone)]
-struct Color(u8, u8, u8);
+#[derive(Copy, Clone, Debug)]
+pub struct Color(u8, u8, u8);
 
-#[derive(Clone)]
-struct Ray {
-    o: Vector3<f32>,
-    d: Vector3<f32>,
+#[derive(Debug)]
+pub struct Ray {
+    pub o: Vector3<f32>,
+    pub d: Vector3<f32>,
 }
 
-#[derive(Clone)]
-struct Tri(Vector3<f32>, Vector3<f32>, Vector3<f32>, Color);
+struct Mesh {
+    pub tris: Vec<Tri>,
+    pub bb: Aabb,
+}
+
+
+#[derive(Copy, Clone, Debug)]
+pub struct Tri {
+    pub a: Vector3<f32>,
+    pub b: Vector3<f32>,
+    pub c: Vector3<f32>,
+    pub color: Color,
+}
+
+pub struct Hit<'a> {
+    pub t: f32,
+    pub tri: &'a Tri,
+}
+
+impl<'a> Hit<'a> {
+    fn new(t: f32, tri: &'a Tri) -> Hit<'a> {
+        Hit { t: t, tri: tri }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Aabb {
+    pub min: Vector3<f32>,
+    pub max: Vector3<f32>,
+}
+
+impl fmt::Display for Aabb {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f,
+               "[{}, {}, {}]..[{}, {}, {}]",
+               self.min.x,
+               self.min.y,
+               self.min.z,
+               self.max.x,
+               self.max.y,
+               self.max.z)
+    }
+}
+
+struct Scene {
+    pub meshes: Vec<Mesh>,
+}
 
 static WIDTH: u32 = 500;
 static HEIGHT: u32 = 500;
@@ -38,6 +90,20 @@ impl Color {
     }
 }
 
+impl Mesh {
+    fn new(mut tris: Vec<Tri>) -> Self {
+        normalize(&mut tris);
+        let bb = compute_aabb(&tris);
+        Mesh {
+            tris: tris,
+            bb: bb,
+        }
+    }
+    fn intersect<'a>(&'a self, r: &Ray) -> Option<Hit<'a>> {
+        intersect(&self.tris, r)
+    }
+}
+
 fn primary_ray(x: u32, y: u32) -> Ray {
     let norm_x = (x as f32 + 0.5) / (WIDTH as f32);
     let norm_y = (y as f32 + 0.5) / (HEIGHT as f32);
@@ -50,165 +116,126 @@ fn primary_ray(x: u32, y: u32) -> Ray {
     };
 }
 
-fn max_dim(v: Vector3<f32>) -> usize {
-    let (x, y, z) = (v.x.abs(), v.y.abs(), v.z.abs());
-    if x > y {
-        if x > z {
-            0
-        } else {
-            2
-        }
-    } else {
-        if y > z {
-            1
-        } else {
-            2
+fn intersect<'a>(tris: &'a [Tri], ray: &Ray) -> Option<Hit<'a>> {
+    let mut closest: Option<Hit> = None;
+    for tri in tris {
+        let corners = (tri.a, tri.b, tri.c);
+        if let Some(t) = watertight_triangle::intersect(corners, ray.o, ray.d) {
+            if closest.is_none() || t < closest.as_ref().unwrap().t {
+                closest = Some(Hit::new(t, tri));
+            }
         }
     }
+    closest
 }
 
-#[allow(non_snake_case)]
-#[inline(never)]
-fn intersect(tri: &Tri, r: &Ray) -> Option<f32> {
-    let (org, dir) = (r.o, r.d);
-    let kz = max_dim(dir);
-    let (kx, ky) = if dir[kz] < 0.0 {
-        ((kz + 1) % 3, (kz + 2) % 3)
-    } else {
-        ((kz + 2) % 3, (kz + 1) % 3)
-    };
-
-    let Sx = dir[kx] / dir[kz];
-    let Sy = dir[ky] / dir[kz];
-    let Sz = 1.0 / dir[kz];
-
-    let (A, B, C) = match *tri {
-        Tri(A, B, C, _) => (A - org, B - org, C - org),
-    };
-    let Ax = A[kx] - Sx * A[kz];
-    let Ay = A[ky] - Sy * A[kz];
-    let Bx = B[kx] - Sx * B[kz];
-    let By = B[ky] - Sy * B[kz];
-    let Cx = C[kx] - Sx * C[kz];
-    let Cy = C[ky] - Sy * C[kz];
-
-    let mut U = Cx * By - Cy * Bx;
-    let mut V = Ax * Cy - Ay * Cx;
-    let mut W = Bx * Ay - By * Ax;
-
-    if U == 0. || V == 0. || W == 0. {
-        let CxBy = (Cx as f64) * (By as f64);
-        let CyBx = (Cy as f64) * (Bx as f64);
-        U = (CxBy - CyBx) as f32;
-        let AxCy = (Ax as f64) * (Cy as f64);
-        let AyCx = (Ay as f64) * (Cx as f64);
-        V = (AxCy - AyCx) as f32;
-        let BxAy = (Bx as f64) * (Ay as f64);
-        let ByAx = (By as f64) * (Ax as f64);
-        W = (BxAy - ByAx) as f32;
-    }
-
-    if (U < 0. || V < 0. || W < 0.) && (U > 0. || V > 0. || W > 0.) {
-        return None;
-    }
-
-    let det = U + V + W;
-    if det == 0. {
-        return None;
-    }
-
-    let Az = Sz * A[kz];
-    let Bz = Sz * B[kz];
-    let Cz = Sz * C[kz];
-    let T = U * Az + V * Bz + W * Cz;
-
-    Some(T / det)
-}
-
-fn trace(r: Ray, scene: &[Tri]) -> Color {
-    let mut hit: Option<&Tri> = None;
-    let mut t_closest_hit = f32::INFINITY;
-    for o in scene.iter() {
-        let t_hit;
-        if let Some(t) = intersect(o, &r) {
-            t_hit = t;
-        } else {
-            continue;
-        }
-        if t_hit < t_closest_hit {
-            t_closest_hit = t_hit;
-            hit = Some(o);
+fn trace(r: Ray, scene: &Scene) -> Color {
+    let mut closest: Option<Hit> = None;
+    for mesh in &scene.meshes {
+        if let Some(hit) = mesh.intersect(&r) {
+            if closest.is_none() || hit.t < closest.as_ref().unwrap().t {
+                closest = Some(hit);
+            }
         }
     }
-    match hit {
-        Some(&Tri(_, _, _, color)) => color,
+    match closest {
+        Some(hit) => hit.tri.color,
         None => BACKGROUND,
     }
 }
 
-fn normalize(vs: &mut [obj::Position]) {
-    // First, compute an AABB for the model
+pub fn compute_aabb(tris: &[Tri]) -> Aabb {
     let mut min = vec3(f32::INFINITY, f32::INFINITY, f32::INFINITY);
     let mut max = -min;
-    for v in vs.iter() {
-        min.x = min.x.min(v.position[0]);
-        min.y = min.y.min(v.position[1]);
-        min.z = min.z.min(v.position[2]);
-        max.x = max.x.max(v.position[0]);
-        max.y = max.y.max(v.position[1]);
-        max.z = max.z.max(v.position[2]);
+    for tri in tris {
+        for v in &[tri.a, tri.b, tri.c] {
+            min.x = min.x.min(v.x);
+            min.y = min.y.min(v.y);
+            min.z = min.z.min(v.z);
+            max.x = max.x.max(v.x);
+            max.y = max.y.max(v.y);
+            max.z = max.z.max(v.z);
+        }
     }
+    Aabb {
+        min: min,
+        max: max,
+    }
+}
+
+fn normalize(tris: &mut [Tri]) {
+    let Aabb { min, max } = compute_aabb(tris);
     let center = (min + max) / 2.0;
     // This heuristically moves the model such that it's probably within view.
     let displace = center + vec3(0.0, 0.0, 1.5 * (min.z - max.z).abs());
-    for v in vs {
-        v.position[0] -= displace.x;
-        v.position[1] -= displace.y;
-        v.position[2] -= displace.z;
+    for tri in tris {
+        tri.a -= displace;
+        tri.b -= displace;
+        tri.c -= displace;
     }
 }
 
-fn read_obj() -> Vec<Tri> {
-    let read = BufReader::new(File::open("bunny.obj").unwrap());
-    let mut o = obj::load_obj::<obj::Position, _>(read).unwrap();
-    normalize(&mut o.vertices);
+fn read_obj<P: AsRef<Path>>(path: P) -> Mesh {
+    let read = BufReader::new(File::open(path).unwrap());
+    let o = obj::load_obj::<obj::Position, _>(read).unwrap();
     let mut tris = Vec::with_capacity(o.indices.len() / 3);
     for chunk in o.indices.chunks(3) {
         assert!(chunk.len() == 3);
-        let i = chunk[0];
-        let j = chunk[1];
-        let k = chunk[2];
-        let tri = Tri(Vector3::from(o.vertices[i as usize].position),
-                      Vector3::from(o.vertices[j as usize].position),
-                      Vector3::from(o.vertices[k as usize].position),
-                      WHITE);
-        tris.push(tri);
+        let (i, j, k) = (chunk[0] as usize, chunk[1] as usize, chunk[2] as usize);
+        tris.push(Tri {
+            a: Vector3::from(o.vertices[i].position),
+            b: Vector3::from(o.vertices[j].position),
+            c: Vector3::from(o.vertices[k].position),
+            color: WHITE,
+        });
     }
-    tris
+    Mesh::new(tris)
+}
+
+fn load_scene() -> Scene {
+    Scene { meshes: vec![read_obj("bunny.obj")] }
 }
 
 fn main() {
-    let scene = read_obj();
+    let sw = Stopwatch::start_new();
+    let scene = load_scene();
     let mut img = bmp::Image::new(WIDTH, HEIGHT);
     let mut pool = Pool::new(4);
+    let mut ray_count = 0;
     pool.scoped(|scope| {
         let coords = img.coordinates();
         let img_root = Arc::new(Mutex::new(&mut img));
         for (i, j) in coords {
             let my_img = img_root.clone();
             let scene = &scene;
+            ray_count += 1;
             scope.execute(move || {
                 let traced = trace(primary_ray(i, j), scene);
                 let mut img = my_img.lock().unwrap();
                 img.set_pixel(i, j, traced.to_px());
-                if i == 0 {
-                    let out = stdout();
-                    let mut out = out.lock();
-                    out.write(b".").unwrap();
-                    out.flush().unwrap();
-                }
             });
         }
     });
     img.save("bunny.bmp").unwrap();
+    println!("");
+    let elapsed = sw.elapsed();
+    println!("Traced {:.2}M rays in {} ({} per ray)",
+             ray_count as f64 / 1e6,
+             pretty_duration(elapsed),
+             pretty_duration(elapsed / ray_count));
+}
+
+fn pretty_duration(d: Duration) -> String {
+    if d.as_secs() > 0 {
+        let secs = d.as_secs() as f64 + d.subsec_nanos() as f64 * 1e-9;
+        return format!("{:.2}s", secs);
+    }
+    let ns = d.subsec_nanos();
+    if ns > 1_000_000 {
+        return format!("{:.2}ms", ns as f64 / 1e6);
+    } else if ns > 1_000 {
+        return format!("{:.2}µs", ns as f64 / 1e3);
+    } else {
+        return format!("{}ns", ns);
+    }
 }
